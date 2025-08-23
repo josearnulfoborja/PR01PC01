9b6eb5f0c3e0878239876bb38a224850d816bad3
Diego Mejía <dmejiaguevara@gmail.com>
2025-08-22 22:04:04 -0600

    Create Reserva.java

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Reserva.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author dmeji
+ */
+public class Reserva {
+    protected String id, fechaSolicitud, inicioReserva, finReserva;
+    protected String reservante, recepcionista, suite, estadoReserva, mensaje;
+    protected boolean estado;
+    
+    public String validarReserva(){
+        if (estado=true)
+            return "RESERVA VALIDA";
+        else 
+            return "RESERVA INVALIDA";
+    }
+}

56d7ce767c88caaf4754101ed90d7054e74930f3
lesslyiraherta <lesslyiraheta@gmail.com>
2025-08-22 22:22:25 -0600

    Clase Cliente
    Clase cliente creada, con lo pedido

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Cliente.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Cliente.java
@@ -1,6 +1,30 @@
 package com.mycompany.mavenproject3;
 
+public class Cliente extends Usuarios {
 
-public class Cliente {
-    
+    private String noTarjeta, tipoCliente, codTarjeta;
+
+    public void setNoTarjeta(String noTarjeta) {
+        this.noTarjeta = noTarjeta;
+    }
+
+    public String getNoTarjeta() {
+        return noTarjeta;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getCodTarjeta() {
+        return codTarjeta;
+    }
+
+    public void setCodTarjeta(String codTarjeta) {
+        this.codTarjeta = codTarjeta;
+    }
 }

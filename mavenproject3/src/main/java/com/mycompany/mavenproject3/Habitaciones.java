e6e68a31f4dd1e5e687f48a2de53bf8afc5056d0
cristiaanx899 <cristiaanx899@gmail.com>
2025-08-23 19:32:18 -0600

    Update Habitaciones.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Habitaciones.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Habitaciones.java
@@ -9,5 +9,60 @@ package com.mycompany.mavenproject3;
  * @author PC
  */
 public class Habitaciones {
+    private int id =0;
+    private int capacidad =0;
+    private String tipo ="SIN ESPECIFICAR";
+    private int nivel =0;
+    private float precio=(float) 0.0;
+    private String estado="SIN ESPECIFICAR";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }

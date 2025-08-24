dd0468867617af3ab1582b11da6b7587ef35f5f6
Diego Mejía <dmejiaguevara@gmail.com>
2025-08-23 20:17:44 -0600

    Update Reserva.java

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/Reserva.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Reserva.java
@@ -4,25 +4,83 @@
  */
 package com.mycompany.mavenproject3;
 
+
 /**
  *
  * @author dmeji
  */
 public class Reserva {
 
-    protected String id, fechaSolicitud, inicioReserva, finReserva;
-    protected String reservante, recepcionista, suite, mensaje;
-    protected boolean estadoReserva;
+    private int id=0;
+    private String fechaSolicitud=null, inicioReserva=null, finReserva=null;
+    private String reservante=null, recepcionista=null, suite=null;
+    public String estadoReserva="INVALIDA", mensaje="SIN MENSAJE";
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public void setFechaSolicitud(String fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public String getInicioReserva() {
+        return inicioReserva;
+    }
+
+    public void setInicioReserva(String inicioReserva) {
+        this.inicioReserva = inicioReserva;
+    }
+
+    public String getFinReserva() {
+        return finReserva;
+    }
+
+    public void setFinReserva(String finReserva) {
+        this.finReserva = finReserva;
+    }
+
+    public String getReservante() {
+        return reservante;
+    }
+
+    public void setReservante(String reservante) {
+        this.reservante = reservante;
+    }
+
+    public String getRecepcionista() {
+        return recepcionista;
+    }
+
+    public void setRecepcionista(String recepcionista) {
+        this.recepcionista = recepcionista;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }    
+    
     public String validarReserva() throws Exception {
         if (reservante == null || recepcionista == null || suite == null) {
             Exception excep = new Exception("No tiene asignado a cliente o empleado o habitacion");
             throw excep;
         } else {
-            if (estadoReserva = true) {
-                return "RESERVA VALIDA";
-            } else {
+            if (estadoReserva == "INVALIDA") {
                 return "RESERVA INVALIDA";
+            } else {
+                return "RESERVA VALIDA";
             }
         }
     }
